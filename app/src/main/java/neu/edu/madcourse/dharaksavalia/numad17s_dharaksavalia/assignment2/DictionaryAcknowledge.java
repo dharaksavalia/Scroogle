2ee0c20a84e45e866dc17e86a35f4610e31562b0
dharaksavalia <dharak.savalia@gmail.com>
2017-02-03 04:23:45 -0500

    everything done including acknowledgements

--- app/src/main/java/neu/edu/madcourse/dharaksavalia/numad17s_dharaksavalia/assignment2/DictionaryAcknowledge.java
+++ app/src/main/java/neu/edu/madcourse/dharaksavalia/numad17s_dharaksavalia/assignment2/DictionaryAcknowledge.java
@@ -19,7 +19,7 @@ public class DictionaryAcknowledge extends Activity {
         String str="Idea"+'\n'+"1. From professor Stephen discussing that ASCII creates redundancy"+'\n'+ "2. http://stackoverflow.com/questions/2276641/way-to-store-a-large-dictionary-with-low-memory-footprint-fast-lookups-on-and\n";
         str+="\nCode Help"+'\n'+"1. https://developer.android.com/reference/android/text/"+'\n'+"2. http://stackoverflow.com/questions/10523801/java-save-binary-code-to-a-file"+'\n';
         str+="\nStrategy "+'\n'+"1. Converting ASCII format to five bits per word (Binary Format) and that storing it raw format. \n2.This not only saves loading time plus space on hard drive ";
-        str+="\n3. Than a preprocessed File containing binary format is loaded into application app starting phase";
+        str+="\n3. Than a preprocessed File containing binary format is loaded into application starting phase";
         textView.setText(str);
     }
 }
